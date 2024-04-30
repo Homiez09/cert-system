1b52709707add3ace8eae97bd30b988025ee9bb1
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2024-04-30 11:01:10 +0700

    add allowedOrigin

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -4,17 +4,28 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
     const data = await request.json()
 
-    if (data.page) { // number
-        const getNews = await fetchNewsByPage(data.page);
-        return NextResponse.json({ ...getNews });
-    } else if (data.recomment) { // boolean
-        const getNews = await fetchNewsByPage(1, 5)
-        return NextResponse.json({ ...getNews });
-    } else if (data.id) {
-        const getNews = await fetchNewsById(data.id);
-        return NextResponse.json({ ...getNews });
-    } else {
-        const getNews = await fetchNews();
-        return NextResponse.json({ ...getNews });
+    try {
+        const allowedOrigin = process.env.NEXT_PUBLIC_UR;
+
+        if (request.headers.get('origin') !== allowedOrigin) {
+            throw new Error('Unauthorized');
+        }
+
+        if (data.page) { // number
+            const getNews = await fetchNewsByPage(data.page);
+            return NextResponse.json({ ...getNews });
+        } else if (data.recomment) { // boolean
+            const getNews = await fetchNewsByPage(1, 5)
+            return NextResponse.json({ ...getNews });
+        } else if (data.id) {
+            const getNews = await fetchNewsById(data.id);
+            return NextResponse.json({ ...getNews });
+        } else {
+            const getNews = await fetchNews();
+            return NextResponse.json({ ...getNews });
+        }
+    } catch (err) {
+        console.error("Error occurred:", err);
+        return NextResponse.json({ error: "An error occurred while processing your request" }, { status: 500 });
     }
 }
