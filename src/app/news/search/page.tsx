dc87ddd2274088119b7aaf29dca22130795367f3
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2024-05-07 19:55:55 +0700

    fixed warning suspense

--- src/app/news/search/page.tsx
+++ src/app/news/search/page.tsx
@@ -1,13 +1,16 @@
 import Card2 from "@/components/new/Card2";
 import { SearchBox } from "@/components/new/SearchBox";
+import { Suspense } from "react";
 
 export default function Page() {
     return (
         <div className="flex flex-col pt-3 w-full max-lg:px-3">
+            <Suspense fallback={<div>Loading...</div>} >
             <SearchBox category="cyberNews"/>
             <div className="h-5 w-full container max-w-5xl mt-5 min-h-[40vh] pt-5">
                 <Card2 />
             </div>
+            </Suspense>
         </div>
     )
 }
