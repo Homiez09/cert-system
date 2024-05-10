4af6037682d110a087bbf771607cbf34f30aa67c
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2024-05-10 12:51:38 +0700

    show category

--- src/components/new/Card2.tsx
+++ src/components/new/Card2.tsx
@@ -5,17 +5,17 @@ import Image from 'next/image';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
-export default ( {props} : {props?: IData}) => {
+export default ({ props }: { props?: IData }) => {
   const router = useRouter();
   const query = useSearchParams().get("query");
 
   useEffect(() => {
     try {
-        let title = document.querySelector(`#title${props!.id}`);
-    
-        title!.innerHTML = props!.attributes.title.replace(new RegExp(query + '(?!([^<]+)?<)', 'gi'), '<mark>$&</mark>');
+      let title = document.querySelector(`#title${props!.id}`);
+
+      title!.innerHTML = props!.attributes.title.replace(new RegExp(query + '(?!([^<]+)?<)', 'gi'), '<mark>$&</mark>');
     } catch (err) {
-        // console.log(err);
+      // console.log(err);
     }
 
   }, [query])
@@ -24,15 +24,15 @@ export default ( {props} : {props?: IData}) => {
     <div className="w-full pb-5 lg:px-5 border-b select-none hover:scale-[1.02] animate-pulse">
       <div className="flex flex-row p-2 hover:cursor-pointer">
         <div className="flex flex-col w-2/3 gap-3">
-          <div className="w-3/4 h-4 bg-gray-300 rounded"/>
-          <div className="w-1/2 h-4 bg-gray-300 rounded"/>
-          <div className="w-1/4 h-4 bg-gray-300 rounded"/>
+          <div className="w-3/4 h-4 bg-gray-300 rounded" />
+          <div className="w-1/2 h-4 bg-gray-300 rounded" />
+          <div className="w-1/4 h-4 bg-gray-300 rounded" />
         </div>
         <div className="flex flex-col items-end w-1/3 p-2">
-          <div className="w-24 h-24 bg-gray-300 rounded-md shadow-md"/>
+          <div className="w-24 h-24 bg-gray-300 rounded-md shadow-md" />
         </div>
       </div>
-    </div>  
+    </div>
   )
   else return (
     <>
@@ -40,15 +40,9 @@ export default ( {props} : {props?: IData}) => {
         <div className="flex flex-row p-2 hover:cursor-pointer" onClick={() => router.push(`/news/cybernews/${props.id}`)}>
           <div className="flex flex-col w-2/3">
             {/* Date */}
-            <small className="text-gray-500">{props.attributes.createdAt.slice(0,10)} | {props.attributes.createdAt.slice(11, 19)}</small>
+            <small className="text-gray-500">{props.attributes.createdAt.slice(0, 10)} | {props.attributes.createdAt.slice(11, 19)}</small>
             {/* Title */}
-            <div id={`title${props.id}`} className="text-xl">{props.attributes.title}</div>
-            <div className="w-full items-center gap-3 block">
-              {/* Description */}
-              <div id={`description${props.id}`} className="text-ellipsis text-wrap overflow-hidden">
-                {/* {props.attributes.content} */}
-              </div>
-            </div>
+            <div id={`title${props.id}`} className="text-2xl">{props.attributes.title}</div>
           </div>
           {/* Image */}
           <div className="flex flex-col items-end w-1/3 p-2">
@@ -62,8 +56,11 @@ export default ( {props} : {props?: IData}) => {
         </div>
         <div className="px-2">
           {/* Categorys */}
-          {/* <CategoryList categorys={props.categorys} /> */}
-
+          {props.attributes.categories.data.map((item, index) => {
+            return (
+              <span key={index} className="text-xs text-gray-500">{item.attributes.name_en}</span>
+            )
+          })}
         </div>
       </div>
     </>
